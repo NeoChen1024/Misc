e185394c54bff13aedf9782ad4f8f6806cdb0371
Neo_Chen <chenkolei@gmail.com>
2026-07-11 15:38:12 +0800

    ackermann.c: use gmp

--- C/ackermann.c
+++ C/ackermann.c
@@ -1,76 +1,179 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <gmp.h>
 #include <signal.h>
+#include <stdatomic.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-typedef unsigned long long int num_t;
+typedef struct {
+	mpz_t *values;
+	size_t size;
+	size_t capacity;
+} virtual_stack;
 
-volatile num_t count=0;
-clock_t start;
-clock_t end=0;
+static atomic_ullong count = 0;
+static clock_t start;
 
-num_t ackermann(num_t m, num_t n)
+static void die(const char *message)
 {
-	count++;
-	if(m == 0)
-		return n+1;
-	if(m > 0 && n == 0)
-		return ackermann(m - 1, 1);
-	if(m > 0 && n > 0)
-		return ackermann(m - 1, ackermann(m, n - 1));
-
-	return 0; /* Might be possible to reach here? */
+	perror(message);
+	exit(EXIT_FAILURE);
 }
 
-void display(union sigval sigval)
+static void stack_init(virtual_stack *stack)
 {
+	stack->values = NULL;
+	stack->size = 0;
+	stack->capacity = 0;
+}
+
+static void stack_grow(virtual_stack *stack)
+{
+	size_t old_capacity = stack->capacity;
+	size_t new_capacity = old_capacity == 0 ? 16 : old_capacity * 2;
+
+	if (new_capacity < old_capacity ||
+	    new_capacity > SIZE_MAX / sizeof(*stack->values)) {
+		errno = ENOMEM;
+		die("virtual stack");
+	}
+
+	mpz_t *new_values = realloc(stack->values,
+	                            new_capacity * sizeof(*stack->values));
+	if (new_values == NULL)
+		die("virtual stack");
+
+	stack->values = new_values;
+	for (size_t i = old_capacity; i < new_capacity; ++i)
+		mpz_init(stack->values[i]);
+	stack->capacity = new_capacity;
+}
+
+static void stack_push(virtual_stack *stack, const mpz_t value)
+{
+	if (stack->size == stack->capacity)
+		stack_grow(stack);
+	mpz_set(stack->values[stack->size++], value);
+}
+
+static void stack_pop(virtual_stack *stack, mpz_t value)
+{
+	mpz_set(value, stack->values[--stack->size]);
+}
+
+static void stack_clear(virtual_stack *stack)
+{
+	for (size_t i = 0; i < stack->capacity; ++i)
+		mpz_clear(stack->values[i]);
+	free(stack->values);
+}
+
+/*
+ * Evaluate Ackermann without using the C call stack.  Each saved m is a
+ * pending invocation.  For A(m, n) with m,n > 0, pushing m-1 followed by m
+ * evaluates the inner A(m, n-1) before its outer continuation A(m-1, ...).
+ */
+static void ackermann(mpz_t result, const mpz_t input_m, const mpz_t input_n)
+{
+	virtual_stack stack;
+	mpz_t m;
+
+	stack_init(&stack);
+	mpz_init(m);
+	mpz_set(result, input_n);
+	stack_push(&stack, input_m);
+
+	while (stack.size != 0) {
+		stack_pop(&stack, m);
+		atomic_fetch_add_explicit(&count, 1, memory_order_relaxed);
+
+		if (mpz_sgn(m) == 0) {
+			mpz_add_ui(result, result, 1);
+		} else if (mpz_sgn(result) == 0) {
+			mpz_set_ui(result, 1);
+			mpz_sub_ui(m, m, 1);
+			stack_push(&stack, m);
+		} else {
+			mpz_sub_ui(result, result, 1);
+			mpz_sub_ui(m, m, 1);
+			stack_push(&stack, m);
+			mpz_add_ui(m, m, 1);
+			stack_push(&stack, m);
+		}
+	}
+
+	mpz_clear(m);
+	stack_clear(&stack);
+}
+
+static void display(union sigval sigval)
+{
+	(void)sigval;
 	clock_t ticks = clock() - start;
-	fprintf(stderr, "\rC=%5.3fk\tT=%5.3fk\t@%5.3f MT/s",
-		(float)count/1000.0,
-		(float)(ticks)/CLOCKS_PER_SEC,
-		(float)(count/ticks));
+	unsigned long long calls =
+	    atomic_load_explicit(&count, memory_order_relaxed);
+	double seconds = (double)ticks / CLOCKS_PER_SEC;
+
+	if (ticks > 0)
+		fprintf(stderr, "\rC=%5.3fk\tT=%5.3fk\t@%5.3f MT/s",
+		        (double)calls / 1000.0, seconds,
+		        (double)calls / (double)ticks);
+}
+
+static int parse_nonnegative(mpz_t value, const char *text)
+{
+	return text[0] != '\0' && text[0] != '-' && mpz_set_str(value, text, 10) == 0;
 }
 
 int main(int argc, char **argv)
 {
-	num_t arg_m, arg_n;
-	num_t ret=0;
-
-	if(argc < 3)
-	{
-		printf("?ARG\n");
-		exit(1);
-	}
-
+	mpz_t arg_m, arg_n, result;
 	timer_t timer;
-	struct sigevent ev =
-	{
+	struct sigevent ev = {
 		.sigev_notify = SIGEV_THREAD,
 		.sigev_notify_function = display,
-		.sigev_notify_attributes = NULL
+		.sigev_notify_attributes = NULL,
 	};
-	timer_create(CLOCK_MONOTONIC, &ev, &timer);
-
-	struct itimerspec period =
-	{
-		.it_value.tv_sec=1,
-		.it_interval.tv_sec=0,
-		.it_interval.tv_nsec=50000000L
+	struct itimerspec period = {
+		.it_value.tv_sec = 1,
+		.it_interval.tv_nsec = 50000000L,
 	};
 
-	timer_settime(timer, TIMER_ABSTIME, &period, NULL);
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s M N\n", argv[0]);
+		return EXIT_FAILURE;
+	}
 
-	arg_m = strtoull(argv[1], NULL, 10);
-	arg_n = strtoull(argv[2], NULL, 10);
+	mpz_inits(arg_m, arg_n, result, NULL);
+	if (!parse_nonnegative(arg_m, argv[1]) ||
+	    !parse_nonnegative(arg_n, argv[2])) {
+		fprintf(stderr, "M and N must be non-negative decimal integers.\n");
+		mpz_clears(arg_m, arg_n, result, NULL);
+		return EXIT_FAILURE;
+	}
+
+	if (timer_create(CLOCK_MONOTONIC, &ev, &timer) == -1)
+		die("timer_create");
+	if (timer_settime(timer, 0, &period, NULL) == -1)
+		die("timer_settime");
 
 	start = clock();
-	ret = ackermann(arg_m, arg_n);
-	end = clock();
-	printf("\nTotal %0.6lf sec, %llu time(s) called, ACKERMANN(%llu, %llu) = %llu\n",
-			(double)(end - start) / 1000000L,
-			count, arg_m, arg_n, ret);
-	printf("%0.2lf c/usec\n", (double)count / (double)(end - start));
-	return 0;
+	ackermann(result, arg_m, arg_n);
+	clock_t end = clock();
+	timer_delete(timer);
+
+	gmp_printf("\nTotal %.6f sec, %llu time(s) called, "
+	           "ACKERMANN(%Zd, %Zd) = %Zd\n",
+	           (double)(end - start) / CLOCKS_PER_SEC,
+	           atomic_load_explicit(&count, memory_order_relaxed), arg_m, arg_n,
+	           result);
+	if (end != start)
+		printf("%.2f c/usec\n",
+		       (double)atomic_load_explicit(&count, memory_order_relaxed) /
+		           (double)(end - start));
+
+	mpz_clears(arg_m, arg_n, result, NULL);
+	return EXIT_SUCCESS;
 }
